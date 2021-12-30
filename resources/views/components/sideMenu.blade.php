5974130f34fb8150aaaaa90065b9c292507a8f7e
senaszel <marekstanislawmeyer@gmail.com>
2021-12-30 17:34:53 +0100

    side-menu css update

--- resources/views/components/sideMenu.blade.php
+++ resources/views/components/sideMenu.blade.php
@@ -2,13 +2,14 @@
 'menuItems'
 ])
 
-<div class="container">
+<link rel="stylesheet" href="{{ asset('css/sideMenu.css') }}">
+
+<div id="sideMenuVerticalContainer" class="container">
     <nav>
-        <ul class="text-center p-2 bg-secondary list-unstyled m-0">
+        <ul id="sideMenuVerticalBarUl">
             @foreach($menuItems as $chosenItem)
-                <li style="margin: 0 0 1rem 0;border: 2px solid black">
+                <li>
                     <a
-                        class="pt-3 pb-3"
                         href="{{ Route('patient-id',['id'=>$chosenItem->id]) }}">
                         {{ $chosenItem->id . ' #id ' . $chosenItem->firstname . ' ' . $chosenItem->lastname . ' (' . $chosenItem->age . ')' }}
                     </a>
