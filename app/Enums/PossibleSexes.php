5db63bf354a55f44b21556500db7726a2a577e8e
senaszel <marekstanislawmeyer@gmail.com>
2021-12-30 23:03:47 +0100

    add enum for PossibleSexes

---
+++ app/Enums/PossibleSexes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+class PossibleSexes
+{
+    const MALE = 'male';
+    const FEMALE = 'female';
+
+    const TYPES = [
+        self::MALE,
+        self::FEMALE,
+    ];
+}
