00572ae63aa7521a2e49c84c9eab06f2feddced5
senaszel <marekstanislawmeyer@gmail.com>
2021-12-30 23:08:50 +0100

    add migration for applications

---
+++ database/migrations/2021_12_30_220558_create_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\ApplicationStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->int('patient_id');
+            $table->int('vaccine_id');
+            $table->int('doctor_id');
+            $table->enum('status',ApplicationStatus::TYPES);
+            $table->timestamp('date_vaccination');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
