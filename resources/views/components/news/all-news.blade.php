27451c1c239cba50ea8ad0355d6a3ba81a0e439c
senaszel <marekstanislawmeyer@gmail.com>
2022-01-06 20:03:49 +0100

    fix on show-all-news components & its css

--- resources/views/components/news/all-news.blade.php
+++ resources/views/components/news/all-news.blade.php
@@ -3,18 +3,22 @@
 <div id="all-news-container">
 
     @foreach($allnews as $news)
-        <a href="{{ url('',['news',$news]) }}" class="abc">
-            <div class="newsMiniature"
-                 style="background-color:
+        <div class="outerlass"
+        >
+            <a href="{{ url('',['news',$news]) }}" class="abc">
+                <div
+                    class="newsMiniature"
+                    style="background-color:
                             {{ \App\Helpers\RandomColor::insert($news->id) }}">
-                <h5>
-                    {{ $news->title }}
-                </h5>
-                <p>
-                    {{ $news->description }}
-                </p>
-            </div>
-        </a>
+                    <h5>
+                        {{ $news->title }}
+                    </h5>
+                    <p>
+                        {{ $news->description }}
+                    </p>
+                </div>
+            </a>
+        </div>
     @endforeach
 
 </div>
