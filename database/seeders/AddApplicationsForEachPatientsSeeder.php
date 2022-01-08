fbd53fcfad4dd62e31002631f57514144e2a96ea
senaszel <marekstanislawmeyer@gmail.com>
2022-01-08 16:37:26 +0100

    fix on AddApplicationsForEachPatientsSeeder: now it inserts only appropriate doctor_ids

--- database/seeders/AddApplicationsForEachPatientsSeeder.php
+++ database/seeders/AddApplicationsForEachPatientsSeeder.php
@@ -6,6 +6,8 @@ use App\Enums\ApplicationStatus;
 use App\Enums\UserRole;
 use App\Models\Application;
 use App\Models\User;
+use App\Models\Vaccine;
+use Exception;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,18 +18,18 @@ class AddApplicationsForEachPatientsSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
-     * @throws \Exception
+     * @throws Exception
      */
     public function run()
     {
         $faker = Factory::create();
         $patients = User::where('role', UserRole::PATIENT)->get('id');
-            foreach ($patients as $i) {
-                while (Application::where('patient_id', $i->id)->count() < 10) {
+            foreach ($patients as $patient) {
+                while (Application::where('patient_id', $patient->id)->count() < 10) {
                     DB::table('applications')->insert([
-                        'patient_id' => $i->id,
-                        'vaccine_id' => random_int(1, 3),
-                        'doctor_id' => random_int(1, User::where('role', UserRole::DOCTOR)->count()),
+                        'patient_id' => $patient->id,
+                        'vaccine_id' => $faker->randomElement(Vaccine::all())->id,
+                        'doctor_id' => $faker->randomElement(User::where('role', UserRole::DOCTOR)->get())->id,
                         'status' => $faker->randomElement(ApplicationStatus::TYPES),
                         'date_vaccination' => $faker->dateTime,
                         'created_at' => now(),
