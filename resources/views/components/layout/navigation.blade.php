3d957ff52eb3e36feeb1fba433d3dbdcbaa5c2e5
senaszel <marekstanislawmeyer@gmail.com>
2022-02-06 21:27:52 +0100

    improvement to navigationComponent & clean its css

--- resources/views/components/layout/navigation.blade.php
+++ resources/views/components/layout/navigation.blade.php
@@ -1,121 +1,113 @@
 <link rel="stylesheet" href="{{ asset('css/navigationComponent.css') }}">
 
-<div id="navContainer"></div>
-<nav id="navNav">
-    <ul id="flex-container" class="nav">
-        <img
-            id="banner" src="{{ asset('banner.svg') }}" alt="banner szczepimy.sie">
-        @guest
-            <li class="navItem">
-                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Aktualności</a>
-            </li>
-        @endguest
+<nav class="navbar navbar-light">
+    <ul class="container-fluid">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img class="img-fluid" id="banner" src="{{ asset('banner.svg') }}" alt="banner szczepimy.sie">
+        </a>
 
         @can('Patient')
-            <li class="navItem">
+            <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Aktualności dla
                     Pacjentów</a>
             </li>
-            <li class="navItem">
-                <a class="nav-link active" aria-current="page"
-                   href="{{ route('patient-applications') }}"
-                >Wnioskuj o szczepienie</a>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('patient-applications') }}">Wnioskuj o
+                    szczepienie</a>
             </li>
-            <li class="navItem">
+            <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('patient-show-applications') }}">Historia
                     szczepień</a>
             </li>
-            <li class="navItem">
+            <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('patient-certificates-index') }}">Moje
                     Certyfikaty szczepienia</a>
             </li>
         @endcan
 
         @can('Admin')
-            <li class="navItem">
-                <div class="dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        Aktualności
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li class="navItem">
-                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Widoczne dla
-                                pacjentów</a>
-                        </li>
-                        <li class="navItem">
-                            <a class="nav-link active" aria-current="page" href="{{ route('create-news') }}">Dodaj nowy
-                                wpis</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="navItem">
-                <div class="dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        Użytkownicy
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li class="navItem">
-                            <a class="nav-link active" aria-current="page"
-                               href="{{ route('index-user',app\Models\User::latest()->first()->id) }}">Wszyscy użytkownicy</a>
-                        </li>
-                        <li class="navItem">
-                            <a class="nav-link active" aria-current="page" href="{{ route('create-user') }}">Dodaj
-                                nowego użytkownika</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Aktualności
+                </a>
+                <ul class="nav-item dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Widoczne dla
+                            pacjentów</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('create-news') }}">Dodaj nowy
+                            wpis</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Użytkownicy
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page"
+                            href="{{ route('index-user', app\Models\User::latest()->first()->id) }}">Wszyscy
+                            użytkownicy</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('create-user') }}">Dodaj
+                            nowego użytkownika</a>
+                    </li>
+                </ul>
+            </div>
         @endcan
 
         @can('Nurse')
-            <li class="navItem">
+            <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('create-patient') }}">Zarejestruj
                     pacjenta</a>
             </li>
-            <li class="navItem">
-                <a class="nav-link active" aria-current="page" href="{{ route('plan-vaccinations') }}">Zaplanuj szczepienia</a>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('plan-vaccinations') }}">Zaplanuj
+                    szczepienia</a>
             </li>
         @endcan
 
         @can('Doctor')
-            <li class="navItem">
-                <a class="nav-link active" aria-current="page" href="{{ route('doctor-work-today') }}">Dzisiejsze szczepienia</a>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('doctor-work-today') }}">Dzisiejsze
+                    szczepienia</a>
             </li>
-            <li class="navItem">
+            <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('vaccines-index') }}">Rejestr
                     szczepionek</a>
             </li>
-            <li class="navItem">
-                <a class="nav-link active" aria-current="page" href="{{ route('doctor-work-schedule') }}">Harmonogram pracy</a>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('doctor-work-schedule') }}">Harmonogram
+                    pracy</a>
             </li>
         @endcan
-        <li class="navItem" id="rightmost">
-            <div class="dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                   data-bs-toggle="dropdown" aria-expanded="false">
-                    @guest
-                        Zacznij tutaj!
-                    @endguest
-                    @auth
-                        {{ auth()->user()->title . ' ' . ucfirst(\App\Models\Personal::where('user_id',auth()->user()->id)->first()->firstname ?? " ") }}
-                        {{ ucfirst(\App\Models\Personal::where('user_id',auth()->user()->id)->first()->lastname ?? "anon") }}
-                    @endauth
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    @guest
-                        <li><a class="dropdown-item" href="{{ route('createNewUser') }}">Załóż konto</a></li>
-                        <li><a class="dropdown-item" href="{{ route('loginForm') }}">Zaloguj</a></li>
-                    @endguest
-                    @auth
-                        <li><a class="dropdown-item" href="{{ route('personals') }}">Moje dane</a></li>
-                        <li><a class="dropdown-item" href="{{ route('logout') }}">Wyloguj</a></li>
-                    @endauth
-                </ul>
-            </div>
-        </li>
+
+        <div class="nav-item dropdown" id="rightmost">
+            <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                @guest
+                    Zacznij tutaj!
+                @endguest
+                @auth
+                    {{ auth()->user()->title .' ' .ucfirst(\App\Models\Personal::where('user_id', auth()->user()->id)->first()->firstname ?? ' ') }}
+                    {{ ucfirst(\App\Models\Personal::where('user_id', auth()->user()->id)->first()->lastname ?? 'anon') }}
+                @endauth
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                @guest
+                    <li><a class="dropdown-item" href="{{ route('createNewUser') }}">Załóż konto</a></li>
+                    <li><a class="dropdown-item" href="{{ route('loginForm') }}">Zaloguj</a></li>
+                @endguest
+                @auth
+                    <li><a class="dropdown-item" href="{{ route('personals') }}">Moje dane</a></li>
+                    <li><a class="dropdown-item" href="{{ route('logout') }}">Wyloguj</a></li>
+                @endauth
+            </ul>
+        </div>
     </ul>
 </nav>
-
